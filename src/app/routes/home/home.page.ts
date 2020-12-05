35c58260a9f863c05ac9be2c85ea078a0a6d1fd6
LeonardYoung <shaojun47@qq.com>
2020-12-05 14:36:53 +0800

    添加首页快捷链接

--- src/app/routes/home/home.page.ts
+++ src/app/routes/home/home.page.ts
@@ -8,6 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
+  public homeItems = [
+    { title: '新增商品', url: '/home', icon: '/assets/img/add_sales.svg' },
+    { title: '新增会员', url: '/home', icon: '/assets/img/add_user.svg' },
+    { title: '收银记账', url: '/home', icon: '/assets/img/sales_account.svg' },
+    { title: '支出管理', url: '/home', icon: '/assets/img/a_note.svg' },
+    { title: '商品管理', url: '/home', icon: '/assets/img/sales_management.svg' },
+    { title: '会员管理', url: '/home', icon: '/assets/img/user_management.svg' },
+    { title: '查询销售', url: '/home', icon: '/assets/img/shop_management.svg' },
+    { title: '智能分析', url: '/home', icon: '/assets/img/analysis.svg' },
+    { title: '供应商管理', url: '/home', icon: '/assets/img/gongying_more.svg' },
+    { title: '挂单', url: '/home', icon: '/assets/img/guandan_more.svg' },
+    { title: '高级功能', url: '/home', icon: '/assets/img/image_addsales.svg' },
+  ];
   private sales: any;
   constructor(private getSaleService: GetSalesService) { }
 
